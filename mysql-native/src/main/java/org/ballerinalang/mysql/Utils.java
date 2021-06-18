4f112df1f304e10ffe0867da74bad427db47bb6a
Danesh Kuruppu <dknkuruppu@gmail.com>
2021-06-18 13:34:58 +0530

    Backport server time zone fix for alpha9 release

--- mysql-native/src/main/java/org/ballerinalang/mysql/Utils.java
+++ mysql-native/src/main/java/org/ballerinalang/mysql/Utils.java
@@ -44,6 +44,24 @@ public class Utils {
             if (socketTimeout > 0) {
                 options.put(Constants.DatabaseProps.SOCKET_TIMEOUT, socketTimeout);
             }
+
+            BString connectionAttributes = mysqlOptions.getStringValue(Constants.Options.CONNECTION_ATTRIBUTES);
+            if (connectionAttributes != null) {
+                options.put(Constants.DatabaseProps.CONNECTION_ATTRIBUTES, connectionAttributes);
+            }
+
+            BString useConfigs = mysqlOptions.getStringValue(Constants.Options.USE_CONFIGS);
+            if (useConfigs != null) {
+                options.put(Constants.DatabaseProps.USE_CONFIGS, useConfigs);
+            }
+
+            boolean allowPublicKeyRetrieval = mysqlOptions.getBooleanValue(Constants.Options.ALLOW_PUBLICKEY_RETRIEVAL);
+            options.put(Constants.DatabaseProps.ALLOW_PUBLICKEY_RETRIEVAL, allowPublicKeyRetrieval);
+
+            BString serverTimezone = mysqlOptions.getStringValue(Constants.Options.SERVER_TIMEZONE);
+            if (serverTimezone != null) {
+                options.put(Constants.DatabaseProps.SERVER_TIMEZONE, serverTimezone);
+            }
             return options;
         }
         return null;
